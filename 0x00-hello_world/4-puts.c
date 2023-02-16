b3adacf823fdf9ab05c346430c85a39bbe22625e
Lempe Lekubu <lempelekubu@gmail.com>
2023-02-16 12:29:53 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
- * main- Printing Programming is like building a multilingual puzzle
- * return: ends with 0
+ * main - print Programming is like building a multilingual puzzle
+ * Return: always 0
  */
 int main(void)
 {
