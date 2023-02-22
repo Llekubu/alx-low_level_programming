f16ea1049054291126adbedb154dab11a85c507a
Lempe Lekubu <lempelekubu@gmail.com>
2023-02-22 13:04:54 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - entry point
+ * description: a function to print out the alphabet in lowercase
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(a);
+	}
+	_putchar('\n');
+}
