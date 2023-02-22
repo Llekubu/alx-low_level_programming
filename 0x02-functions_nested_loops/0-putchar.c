c28be365ad8f09e3e1a51a095e1ee90a65b84925
Lempe Lekubu <lempelekubu@gmail.com>
2023-02-22 07:38:38 -0800

    0-putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,20 @@
 #include "main.h"
 
 /**
- * main - Entry point
- * Print _putchar followed by new line
- * Return: always 0
+ * main -Entry point
+ * Return: Success 0
  */
+
 int main(void)
 {
-	char a[] = "_putchar";
-	int c = 0;
-
-	while (a[c] != '\0')
-	{
-		_putchar(a[c]);
-		c++;
-	}
-	_putchar(10);
-
-	return (0);
+        _putchar('_');
+        _putchar('p');
+        _putchar('u');
+        _putchar('t');
+        _putchar('c');
+        _putchar('h');
+        _putchar('a');
+        _putchar('r');
+        _putchar('\n');
+        return (0);
 }
