ce468035fc56c1bbc7be6ee21c1ea2cb8fa43347
Lempe Lekubu <lempelekubu@gmail.com>
2023-02-22 13:51:28 -0800

    8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints minutes of the day
+ * From 00:00 TO 23:59
+ */
+void jack_bauer(void)
+{
+	int n, g;
+
+	n = 0;
+
+	while (n < 24)
+	{
+		g = 0;
+		while (g < 60)
+		{
+			_putchar((n / 10) + '0');
+			_putchar((n % 10) + '0');
+			_putchar(':');
+			_putchar((g / 10) + '0');
+			_putchar((g % 10) + '0');
+			_putchar('\n');
+			g++;
+		}
+		n++;
+	}
+}
+
